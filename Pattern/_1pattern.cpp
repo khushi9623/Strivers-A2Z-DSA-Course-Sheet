f165d4a2ee1d60ea57256193d482fdafc4ccf6df
Khushi <rathikhushi08@gmail.com>
2023-01-06 03:32:49 +0530

    Some changes are done

--- Pattern/_1pattern.cpp
+++ Pattern/_1pattern.cpp
@@ -10,18 +10,7 @@ Any pattern can be solved using the following rules:
 4) Take note of the symmetry (optional)
 
 */
-/*
-Any pattern can be solved using the following rules:
 
-1) The outer loop is always for rows; count the number of rows and determine the range accordingly.
-
-2)internal loop concentrate on columns and connect them to rows in some way
-
-3)Print whatever you want in the inner loop.
-
-4) Take note of the symmetry (optional)
-
-*/
 //{ Driver Code Starts
 #include <bits/stdc++.h>
 
